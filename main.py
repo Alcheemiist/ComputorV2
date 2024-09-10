66f3f0234ce0b04807037ac0dadb8654fb059894
Eelaazmi <elaazmielmahdi@gmail.com>
2024-09-10 15:45:58 +0100

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     context = {}  # Store variables and functions
+    # history = []  # Store history of user input and results
 
     DEBUG = sys.argv[1] if len(sys.argv) > 1 else False
     DEBUG = DEBUG == "--debug"
@@ -15,17 +16,24 @@ def main():
         user_input = get_user_input()
         if user_input == "exit" or user_input == "quit" or user_input == "q!":
             break
+        if user_input == "history":
+            for i, (input, result) in enumerate(history):
+                print(f"{i+1}. {input} = {result}")
+            continue
         try:
             DEBUG and print("Input:", user_input)
             tokens = lexer(user_input, DEBUG)
 
-            continue
-
+            # continue
             ast = parser(tokens)
             result = evaluator(ast, context)
             # print(result)
+            # history.append((user_input, result))
+
+
         except Exception as e:
             print(f"Error: {e}")
+        
 
 if __name__ == "__main__":
     main()
