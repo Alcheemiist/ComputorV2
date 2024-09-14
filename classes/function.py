9aab120d1dee6ab18f8d8e9a6116c4c0456333ab
Eelaazmi <elaazmielmahdi@gmail.com>
2024-09-14 15:57:57 +0100

    structure

--- classes/function.py
+++
@@ -1,9 +0,0 @@
-
-# function.py
-class Function:
-    # Implementation for functions
-    def __init__(self, name, args, body):
-        self.name = name
-        self.args = args
-        self.body = body
-        print("Function created")
